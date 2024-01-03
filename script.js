a0fa720053d6a3eadf1ed3ef5d3ad393667034ac
Aishu <aishu.chinnaiya1112@gmail.com>
2024-01-03 19:36:46 +0800

    code to match card

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const cards = document.querySelectorAll(".memory-card")
-// console.log(cards)
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle("flip")
+    this.classList.add("flip")
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
 }
+
+ function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+
+    unFlipCards();
+ }
+
+ function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard)
+ }
+
+ function unFlipCards() {
+    setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+ }, 1500);
+ }
 
 cards.forEach(card => card.addEventListener("click", flipCard))
